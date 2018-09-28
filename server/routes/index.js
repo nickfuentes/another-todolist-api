7583e85f8ed345760cac5a925a87065b1f8e0ca2
Nickolas Fuentes <>
2018-09-27 23:14:46 -0500

    Created update and destroy routes and controllers for single todoitems

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,11 +6,20 @@ module.exports = (app) => {
         message: 'Welcome to the Todos API!',
     }));
 
+    //Todos
     app.post('/api/todos', todosController.create);
     app.get('/api/todos', todosController.list);
     app.get('/api/todos/:todoId', todosController.retrieve);
     app.put('/api/todos/:todoId', todosController.update);
     app.delete('/api/todos/:todoId', todosController.destroy);
 
+    //TodoItems
     app.post('/api/todos/:todoId/items', todoItemsController.create);
+    app.put('/api/todos/:todoId/items/:todoItemId', todoItemsController.update);
+    app.delete('/api/todos/:todoId/items/:todoItemId', todoItemsController.destroy);
+
+    app.all('/api/todos/:todoId/items', (req, res) =>
+        res.status(405).send({
+            message: 'Method Not Allowed',
+        }));
 };
