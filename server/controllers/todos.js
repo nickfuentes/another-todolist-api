50ff0ee8199e0b6f9a0c8a73955844a9ec1752b3
Nickolas Fuentes <>
2018-09-27 22:23:17 -0500

    Created find single todo route and controller

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,4 +1,5 @@
 const Todo = require('../models').Todo;
+const TodoItem = require('../models').TodoItem;
 
 module.exports = {
     create(req, res) {
@@ -12,8 +13,32 @@ module.exports = {
 
     list(req, res) {
         return Todo
-            .all()
+            .findAll({
+                include: [{
+                    model: TodoItem,
+                    as: 'todoItems',
+                }],
+            })
             .then(todos => res.status(200).send(todos))
+            .catch(error => res.status(400).send(error));
+    },
+
+    retrieve(req, res) {
+        return Todo
+            .findById(req.params.todoId, {
+                include: [{
+                    model: TodoItem,
+                    as: 'todoItems',
+                }],
+            })
+            .then(todo => {
+                if (!todo) {
+                    return res.status(404).send({
+                        message: 'Todo Not Found',
+                    });
+                }
+                return res.status(200).send(todo);
+            })
             .catch(error => res.status(400).send(error));
     },
 };
