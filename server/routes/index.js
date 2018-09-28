dd632c195358cea7827eb3445526c216c3bc32d1
Nickolas Fuentes <>
2018-09-27 22:34:47 -0500

    created updated single todo route and controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,7 @@ module.exports = (app) => {
     app.post('/api/todos', todosController.create);
     app.get('/api/todos', todosController.list);
     app.get('/api/todos/:todoId', todosController.retrieve);
+    app.put('/api/todos/:todoId', todosController.update);
 
     app.post('/api/todos/:todoId/items', todoItemsController.create);
 };
